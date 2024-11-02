198dd9d464606bd0654830e7fe5159cac43c89d1
Ariel Rahmane <ariel.rahmane@leonardo.ai>
2024-11-02 17:55:00 +1100

    feat: no need for manual type check

--- src/code-parser.ts
+++ src/code-parser.ts
@@ -9,12 +9,20 @@ const sourceFile = project.addSourceFileAtPath(filePath);
 
 const chunks: Array<{
   type: string;
-  details: string;
+  width: number;
+  position: number;
+  startLineNumber: number;
   code: string;
 }> = [];
 
-function addChunk(type: string, details: string, code: string) {
-  chunks.push({ type, details, code });
+function addChunk(
+  type: string,
+  width: number,
+  position: number,
+  startLineNumber: number,
+  code: string
+) {
+  chunks.push({ type, width, position, startLineNumber, code });
 }
 
 const handlerVariable = sourceFile.getVariableDeclaration("handler");
@@ -25,21 +33,13 @@ const handlerFunction = handlerVariable?.getInitializerIfKindOrThrow(
 if (handlerFunction) {
   let index = 0;
   handlerFunction.getBody().forEachChild((node) => {
-    if (Node.isIfStatement(node)) {
-      addChunk("if statement", "Conditional Check", node.getText());
-    } else if (Node.isSwitchStatement(node)) {
-      addChunk("switch statement", "Switch Case Check", node.getText());
-    } else if (Node.isVariableDeclaration(node)) {
-      const varName = node.getName();
-      addChunk("variable declaration", `Variable: ${varName}`, node.getText());
-    } else if (Node.isAwaitExpression(node)) {
-      addChunk("await expression", "Asynchronous Action", node.getText());
-    } else if (Node.isCallExpression(node)) {
-      const funcName = node.getExpression().getText();
-      addChunk("function call", `Function Call: ${funcName}`, node.getText());
-    } else {
-      addChunk("other", "Other type of chunk", node.getText());
-    }
+    addChunk(
+      node.getKindName(),
+      node.getFullWidth(),
+      node.getPos(),
+      node.getStartLineNumber(),
+      node.getText()
+    );
     index++;
   });
 }
